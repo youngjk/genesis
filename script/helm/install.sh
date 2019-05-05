0a38c4235e57789841ee9262e4020461582e61bd
Young Jin Kim <youngjin.kim@universe.com>
2019-05-04 21:11:51 -0400

    Added separators & minor modification to account for different types of helm-values dir structure

--- script/helm/install.sh
+++ script/helm/install.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
+SEPERATOR="----------------------------------------"
+
 # name of helm chart/deployment
+echo -e "$SEPERATOR"
 echo -e "$(ls ~/Universe/unii-helm-charts/charts)"
 echo -e "Project: "
 read project
 
 # name of release/deployment
+echo -e "$SEPERATOR"
 echo -e "Release Name: "
 read release
 
 # name of kube context/project
-echo -e "$(gcloud projects list | tail -n +2 | awk '{print $2}')"
+echo -e "$SEPERATOR"
+echo -e "$(kubectl config get-contexts | tail -n +2 | awk '{print $2}')"
 echo -e "Context:"
 read context
 
 # name of namespace
+echo -e "$SEPERATOR"
 echo -e "Namespace:"
 read namespace
 
 # sha of release/deployment
+echo -e "$SEPERATOR"
 echo -e "Project sha:"
 read sha
 
+echo -e "$SEPERATOR"
+echo -e "Starting Helm install"
 helm secrets clean ~/Universe/unii-helm-charts/helm-values/"$project";
-helm secrets dec ~/Universe/unii-helm-charts/helm-values/"$project"/secrets.yaml;
-helm install ~/Universe/unii-helm-charts/charts/"$project" --name "$release" --kube-context "$context" --namespace "$namespace" --set env="$context" -f ~/Universe/unii-helm-charts/helm-values/"$project"/secrets.yaml.dec --set image.tag="$sha"
+if [[ $(ls ~/Universe/unii-helm-charts/helm-values/$project | grep "secrets.yaml") ]]; then
+  helm secrets dec ~/Universe/unii-helm-charts/helm-values/"$project"/secrets.yaml;
+  helm install ~/Universe/unii-helm-charts/charts/"$project" --name "$release" --kube-context "$context" --namespace "$namespace" --set env="$context" -f ~/Universe/unii-helm-charts/helm-values/"$project"/secrets.yaml.dec --set image.tag="$sha"
+else
+  helm secrets dec ~/Universe/unii-helm-charts/helm-values/"$project"/"$context"/secrets.yaml;
+  helm install ~/Universe/unii-helm-charts/charts/"$project" --name "$release" --kube-context "$context" --namespace "$namespace" --set env="$context" -f ~/Universe/unii-helm-charts/helm-values/"$project"/"$context"/secrets.yaml.dec --set image.tag="$sha"
+fi
