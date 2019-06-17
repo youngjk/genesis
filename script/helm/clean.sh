95cd0862fabefbdf3c43c81f09c72e7ec01c8fa5
Young Jin Kim <youngjin.kim@universe.com>
2019-06-16 22:46:49 -0400

    Refactored helm clean script

--- script/helm/clean.sh
+++ script/helm/clean.sh
@@ -1,8 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# This script is to clean up all orphaned helm releases
+## clean.sh is a script that cleans up all orphaned helm releases
 
+# Fixed variables
 SEPERATOR="----------------------------------------"
+
+# Used cli check
+if ! command -v kubectl > /dev/null; then
+  echo "'kubectl' is not installed" >&2 && \
+  exit 1
+fi
+
+if ! command -v helm > /dev/null; then
+  echo "'helm' is not installed" >&2 && \
+  exit 1
+fi
 
 # Returns list of namespaces in kube-context
 KUBE_NAMESPACES=$(kubectl get namespaces | tail -n +2 | awk '{print $1}')
@@ -16,28 +28,27 @@ echo -e "$SEPERATOR"
 echo -e "Namespaces in helm list:"
 echo "${HELM_NAMESPACES[@]}"
 
-# list of orphaned release namespaces
+# Compute and return list of orphaned release namespaces
 DESTROYED_NAMESPACES=()
-
-# Compute list of orphaned release namespaces
 for i in $HELM_NAMESPACES; do
-  found=0
-  for j in $KUBE_NAMESPACES; do
-    [[ $i == $j ]] && found=1
-  done
-  [[ $found == 1 ]] || DESTROYED_NAMESPACES+=($i)
+  if [[ ! "${KUBE_NAMESPACES[@]} =~ $i" ]]; then
+    DESTROYED_NAMESPACES+=($i)
+  fi
 done
 
 echo -e "$SEPERATOR"
 echo -e "Orphaned Namespace with Releases:"
 echo "${DESTROYED_NAMESPACES[@]}" | tr ' ' '\n'
 
+# Prompt user whether to delete orphaned releases
 echo -e "$SEPERATOR"
-echo -e "Should we proceed with deleting orphaned releases? (Y/N)"
+echo -e "Should we proceed with deleting orphaned releases? (Y\N)"
 read proceed
 # Purge delete orphaned releases
-if [[ $proceed == "Y" ]]; then
+if [[ ${proceed,,} == "y" ]]; then
   for i in ${DESTROYED_NAMESPACES[@]}; do
     helm delete --purge $(helm list -a -m 1000 --namespace $i | awk '{print $1}' | tail -n +2)
   done
+else
+  echo -e "Exiting program"
 fi
