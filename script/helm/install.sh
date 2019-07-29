9e9bf0f790728805b6764929fb51ed1ee19616ea
Young Jin Kim <youngjin.kim@universe.com>
2019-07-28 21:45:17 -0400

    Updated helm install shell script to be more compatible with all charts

--- script/helm/install.sh
+++ script/helm/install.sh
@@ -4,12 +4,18 @@
 
 # Fixed variables
 SEPERATOR="----------------------------------------"
+HELM_CHARTS_PATH=~/Universe/unii-helm-charts/charts
+HELM_VALUES_PATH=~/Universe/unii-helm-charts/helm-values
 
 # Fetch chart names and prompt user to select project to deploy
 echo -e "$SEPERATOR"
-echo -e "$(ls ~/Universe/unii-helm-charts/charts)"
+echo -e $(ls $HELM_CHARTS_PATH) | tr ' ' '\n'
 echo "Project: "
 read project
+# Check if project exists
+if [[ ! $(ls $HELM_CHARTS_PATH/$project) ]]; then
+  exit 1
+fi
 
 # Prompt user for deployment name
 echo -e "$SEPERATOR"
@@ -18,7 +24,7 @@ read release
 
 # Prompt user for kube context
 echo -e "$SEPERATOR"
-echo -e "$(kubectl config get-contexts | tail -n +2 | awk '{print $2}')"
+echo -e "$(kubectl config get-contexts | tail -n +2 | awk -F '[[:space:]]' '!$1{ $1="NA" }1' | awk '{print $2}')"
 echo "Context: "
 read context
 
@@ -32,28 +38,45 @@ echo -e "$SEPERATOR"
 echo "Project sha: "
 read sha
 
+# Determine secrets path if it exists
+echo -e "$SEPERATOR"
+helm secrets clean $HELM_VALUES_PATH;
+if [[ $(ls $HELM_VALUES_PATH/$project | grep "secrets.yaml") ]]; then
+  helm secrets dec $HELM_VALUES_PATH/$project/"secrets.yaml"
+  secrets_path=$HELM_VALUES_PATH/$project/secrets.yaml.dec
+elif [[ $(ls $HELM_VALUES_PATH/$project/$context | grep "secrets.yaml") ]]; then
+  helm secrets dec $HELM_VALUES_PATH/$project/$context/secrets.yaml
+  secrets_path=$HELM_VALUES_PATH/$project/$context/secrets.yaml.dec
+else
+  echo -e "There is no secrets for this project"
+fi
+
+# Determine overwrite helm-values path if it exists
+echo -e "$SEPERATOR"
+if [[ $(ls $HELM_VALUES_PATH/$project/$context | grep "values.yaml") ]]; then
+  overwrite_values_path=$HELM_VALUES_PATH/$project/$context/values.yaml
+else
+  echo -e "There is no overwrite values for this project"
+fi
+
 # Start helm install to deploy project with specified arguments
 echo -e "$SEPERATOR"
 echo -e "Starting Helm install"
-helm secrets clean ~/Universe/unii-helm-charts/helm-values/"$project";
-if [[ $(ls ~/Universe/unii-helm-charts/helm-values/$project | grep "secrets.yaml") ]]; then
-  helm secrets dec ~/Universe/unii-helm-charts/helm-values/"$project"/secrets.yaml;
-  helm install ~/Universe/unii-helm-charts/charts/"$project" \
-    --name "$release" \
-    --kube-context "$context" \
-    --namespace "$namespace" \
-    --set env="$context" \
-    -f ~/Universe/unii-helm-charts/helm-values/"$project"/secrets.yaml.dec \
-    -f ~/Universe/unii-helm-charts/helm-values/"$project"/values.yaml \
-    --set image.tag="$sha"
-elif [[ $(ls ~/Universe/unii-helm-charts/helm-values/$project/$context | grep "secrets.yaml") ]]; then
-  helm secrets dec ~/Universe/unii-helm-charts/helm-values/"$project"/"$context"/secrets.yaml;
-  helm install ~/Universe/unii-helm-charts/charts/"$project" \
-    --name "$release" \
-    --kube-context "$context" \
-    --namespace "$namespace" \
-    --set env="$context" \
-    -f ~/Universe/unii-helm-charts/helm-values/"$project"/"$context"/secrets.yaml.dec \
-    -f ~/Universe/unii-helm-charts/helm-values/"$project"/"$context"/values.yaml \
-    --set image.tag="$sha"
+args=(
+  --name $release
+  --kube-context $context
+  --namespace $namespace
+  --set env=$context
+  --set image.tag=$sha
+)
+if [[ ! -z $secrets_path ]]; then
+  echo -e "SECRETS PATH: $secrets_path"
+  args+=(-f $secrets_path)
 fi
+
+if [[ ! -z $overwrite_values_path ]]; then
+  echo -e "VALUES PATH: $overwrite_values_path"
+  args+=(-f $overwrite_values_path)
+fi
+
+helm install $HELM_CHARTS_PATH/$project ${args[@]}
